4dad7a311eb3222c908334c7f56883a26e1a1ce7
MTariqR <5100155@mypillar7uwc.ac.za>
2023-12-08 03:15:45 +0200

    Add project files.

---
+++ RealWorldClassObject/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RealWorldClassObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person noah = new Person("Noah",26,"Brown","Brown",176,true,true);
+        }
+
+    }
+}
---
+++ RealWorldClassObject/RealWorldObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealWorldClassObject
+{
+    internal class Person
+    {
+        public string name;
+        public int age;
+        public string eyeColour;
+        public string hairColour;
+        public double heightInMetres;
+        public bool clothes;
+        public bool eyesOpen;
+
+        public Person(string Name, int Age, string EyeColour, string HairColour, double HeightInMetres, bool Clothes, bool EyesOpen) 
+        {
+            name = Name;
+            age = Age;
+            eyeColour = EyeColour;
+            hairColour = HairColour;
+            heightInMetres = HeightInMetres;
+            clothes = Clothes;
+            eyesOpen = EyesOpen;
+        }
+    }
+}
